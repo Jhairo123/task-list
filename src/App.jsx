36fc2caeffb0f0e47e135aac445f0507492da89d
Nicolas Gómez <gomez.reyes.jhair@gmail.com>
2023-09-29 13:30:58 -0500

    add TaskList

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Componentes/Header";
+import TaskList from "./Componentes/TaskList";
+
+function App() {
+  // Lista es un array de objetos
+
+  return (
+    <div>
+      <Header />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
