e3ed96857ab66f6a598f5d7c0898f17cd3d0ea9d
Nicolas Gómez <gomez.reyes.jhair@gmail.com>
2023-12-08 23:46:29 -0500

    update react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Componentes/Header";
-import TaskList from "./Componentes/TaskList";
+import TaskList from "./pages/TaskList";
 import { MyContext } from "./createContext";
 import { useTask } from "./customHook";
+import { HomePage } from "./pages/HomePage";
+import { AboutUs } from "./pages/AboutUs";
+import { Menu } from "./Componentes/Menu";
+
+export function AppRouter() {
+  return (
+    <>
+      <Menu />
+      <Routes>
+        <Route path="/Home" element={<HomePage />} />
+        <Route path="/" element={<TaskList />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   const [
@@ -21,27 +39,30 @@ function App() {
   ] = useTask();
 
   return (
-    <MyContext.Provider
-      value={{
-        handleButtonAdd,
-        handleTaskEdit,
-        handleTaskDelete,
-        handleButtonDelete,
-        dataList,
-        setDataList,
-        inputValue,
-        setInputValue,
-        textAreaValue,
-        setTextAreaValue,
-        handleCheckboxChange,
-        isChecked,
-      }}
-    >
-      <div>
-        <Header />
-        <TaskList />
-      </div>
-    </MyContext.Provider>
+    <Router>
+      <MyContext.Provider
+        value={{
+          handleButtonAdd,
+          handleTaskEdit,
+          handleTaskDelete,
+          handleButtonDelete,
+          dataList,
+          setDataList,
+          inputValue,
+          setInputValue,
+          textAreaValue,
+          setTextAreaValue,
+          handleCheckboxChange,
+          isChecked,
+        }}
+      >
+        <div>
+          <AppRouter />
+          {/* <Header /> */}
+          {/* <TaskList /> */}
+        </div>
+      </MyContext.Provider>
+    </Router>
   );
 }
 
