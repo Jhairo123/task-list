439489ac4da6876b6fd225e092449e8907028254
Nicolas Gómez <gomez.reyes.jhair@gmail.com>
2023-12-11 00:24:48 -0500

    change(content) page AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,3 +1,72 @@
 export function AboutUs() {
-  return <main>About Us</main>;
+  return (
+    <>
+      <p>
+        Esta aplicación fue contruida por
+        <a href="https://github.com/Jhairo123"> Nicolas Gomez</a>.
+        <br />
+        Él es un desarrollador Colombiano, que puso a prueba sus conocimiento en
+        esta aplicación (<i>lista de tareas</i>) usando las siguientes
+        tecnologias:
+      </p>
+      <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+          alt="html5"
+          width="40"
+          height="40"
+        />
+      </a>
+      <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+          alt="css3"
+          width="40"
+          height="40"
+        />
+      </a>
+
+      <a
+        href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+          alt="javascript"
+          width="40"
+          height="40"
+        />
+      </a>
+      <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+        <img
+          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+          alt="react"
+          width="40"
+          height="40"
+        />
+      </a>
+      <p>
+        Esta aplicación es <i>responsive</i>, es decir, que también puede ser
+        utilizada en dispositivos móviles.
+        <br />
+        Si deseas saber mas acerca de él, este es su perfil en
+        <a href="https://github.com/Jhairo123"> Git Hub</a>.
+      </p>
+      <div className="container">
+        <blockquote>
+          "Mi viaje en el mundo de la programación comenzó cuando era niño;
+          siempre he sentido pasión por la forma en que los algoritmos han
+          agilizado nuestros procesos, permitiéndonos así ahorrar tiempo.
+          <br />
+          Mi curiosidad por aprender más al respecto me llevó a sumergirme en el
+          mundo del desarrollo, y desde entonces, he estado en una búsqueda
+          continua enriqueciendo mis conocimientos y habilidades."
+          <br />
+          Mi propósito es contribuir significativamente a una sociedad mejor,
+          más avanzada, óptima y eficaz.
+        </blockquote>
+      </div>
+    </>
+  );
 }
