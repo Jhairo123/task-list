ef8eeccba7c5011f7380070e7150cf6d9b01e3b7
Nicolas Gómez <gomez.reyes.jhair@gmail.com>
2024-01-05 13:13:02 -0500

    feat(color mode): night mode

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,36 +1,78 @@
-import { Link } from "react-router-dom";
-import "../styles/header.css";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+import { WiDaySunny } from "react-icons/wi";
+import { Link as ReactRouterLink } from "react-router-dom";
+import {
+  Button,
+  Link as ChakraLink,
+  IconButton,
+  useColorMode,
+} from "@chakra-ui/react";
+import { Box, Flex, Heading, Image } from "@chakra-ui/react";
+// import "../styles/header.css";
+
 import logoImg from "../img/img.jpg";
-import menuImg from "../img/menu.png";
+// import menuImg from "../img/menu.png";
 /* eslint-disable react/prop-types */
 export default function Header() {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <header className="header">
-      <div className="container">
-        <div className="content-logo">
-          <img src={logoImg} alt="Logo" />
-          <h1>Lista de tareas</h1>
-        </div>
-        <div className="boton">
-          <label htmlFor="btn-menu">
-            <img className="img-menu" src={menuImg} alt="Logo" />
-          </label>
-        </div>
-        <input type="checkbox" id="btn-menu" />
-        <nav className="nav-menu">
-          <Link className="li" to="/Home">
-            Home
-          </Link>
-
-          <Link className="li" to="/">
+    <Box
+      as="header"
+      bgColor="#101922"
+      boxShadow="0px 1px 10px rgba(0, 0, 0, 0.4)"
+      position="fixed"
+      zIndex="10"
+      top="0"
+      left="0"
+      width="100%"
+      height="65px"
+    >
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        width="90%"
+        maxW="1500px"
+        mx="auto"
+      >
+        <Flex align="center">
+          <Image borderRadius="50%" boxSize="50px" src={logoImg} alt="Logo" />
+          <Heading
+            as="h1"
+            display="inline"
+            position="fixed"
+            my="15px"
+            ml="65px"
+            color="#fff"
+            fontWeight="400"
+            fontSize="24px"
+            cursor="default"
+          >
             Lista de tareas
-          </Link>
+          </Heading>
+        </Flex>
 
-          <Link className="li" to="/AboutUs">
-            About Us
-          </Link>
-        </nav>
-      </div>
-    </header>
+        <Flex flexDirection="row" gap={5} my={3}>
+          <IconButton
+            colorScheme={colorMode === "light" ? "yellow" : "messenger"}
+            // mt="6px"
+            // color="black"
+            border="3px solid white"
+            borderRadius="30px"
+            size="md"
+            onClick={toggleColorMode}
+            icon={colorMode === "light" ? <SunIcon /> : <MoonIcon />}
+          ></IconButton>
+          <ChakraLink mt="7px" color="white" as={ReactRouterLink} to="/Home">
+            Inicio
+          </ChakraLink>
+          <ChakraLink mt="7px" color="white" as={ReactRouterLink} to="/">
+            Lista de tareas
+          </ChakraLink>
+          <ChakraLink mt="7px" color="white" as={ReactRouterLink} to="/AboutUs">
+            Sobre mi
+          </ChakraLink>
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
