98f4551305e2fc2e8c76f5c0d10d6936cbe8f2ef
Nicolas Gómez <gomez.reyes.jhair@gmail.com>
2024-01-05 13:15:39 -0500

    feat(ux/ui): Desing system with chakra UI

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import { useContext, useState } from "react";
 import { MyContext } from "../createContext";
+import { useForm } from "react-hook-form";
+import { FaRegEdit } from "react-icons/fa";
 import {
   Button,
   Textarea,
@@ -9,13 +11,35 @@ import {
   Image,
   Grid,
   Flex,
-  grid,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Tooltip,
 } from "@chakra-ui/react";
 
 import { Switch } from "@chakra-ui/react";
+import { useBoolean } from "@chakra-ui/react";
+
+import editImg from "../img/edit.png";
 
 export default function Task({ id, title, description, state }) {
-  const [isVisible, setIsVisible] = useState(true);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    // delayError: 1000,
+    defaultValues: {
+      title: title,
+      description: description,
+    },
+  });
+  const [visibility, setVisibility] = useBoolean(true);
+  // const [visibility, setIsVisible] = useState(true);
   const [editField, setEditField] = useState({
     title: title,
     description: description,
@@ -24,10 +48,13 @@ export default function Task({ id, title, description, state }) {
     useContext(MyContext);
 
   const handleToggleVisibility = () => {
-    setIsVisible(!isVisible);
+    setVisibility.toggle();
+    // console.log(visibility);
+
+    // setIsVisible(!isVisible);
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSendSubmit = (e) => {
+    // e.preventDefault();
     handleTaskEdit(id, editField);
     handleToggleVisibility();
   };
@@ -41,8 +68,8 @@ export default function Task({ id, title, description, state }) {
   };
 
   return (
-    <Box
-      w="350px"
+    <Grid
+      // w="20px"
       marginLeft={2}
       // marginRight={1}
       // display="inline-block"
@@ -50,77 +77,147 @@ export default function Task({ id, title, description, state }) {
       overflow="auto"
       backgroundColor="whitesmoke"
       borderRadius="1rem"
-      id={isVisible ? "content-task" : "content-edit-task"}
+      height={visibility ? "6rem" : "12.5rem"}
     >
-      <Box
-        display="grid"
-        gridTemplateColumns="15% 85%"
+      <Grid
+        gridTemplateColumns="15%"
         // paddingRight={90}
-        className={isVisible ? "content-show" : "content-hiden"}
+        display={visibility ? "grid" : "none"}
       >
-        <Box gridColumnStart="1" border="1px transparent">
+        <Grid
+          // my={1}
+          placeContent="center"
+          gridColumn="1 / span 1"
+          border="1px transparent"
+        >
           {/* imagen editar */}
           <Image
-            marginTop={1.5}
-            // marginBottom={0}
+            // marginTop={1}
+            cursor="pointer"
+            marginTop={0}
             boxSize="40px"
             // className="imagen"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcgSizhklDH657yz3n7DSH6nCripsKtMhds9M8xic_cZm04rHSkwiNcg_fg502AWSC48g&usqp=CAU"
+            src={editImg}
+            // src=""
             onClick={() => handleToggleVisibility()}
           />
           {/* imagen eliminar */}
           <Image
-            // marginTop={1}
+            marginBottom={0.5}
+            cursor="pointer"
             // marginBottom={0}
             boxSize="40px"
             className="imagen"
             src="https://png.pngtree.com/png-clipart/20220926/original/pngtree-delete-button-3d-icon-png-image_8633077.png"
             onClick={() => handleTaskDelete(id)}
           />
-        </Box>
-        <Box gridColumnStart="2" border="1px solid">
-          <Box display="grid" gridTemplateColumns="19% 90%">
+        </Grid>
+        <Grid gridColumn="2 / span 1" border="1px transparent">
+          <Grid
+            // my={1}
+            border="1px transparent"
+            gridTemplateColumns="19% 1fr"
+            gridTemplateRows="auto auto"
+          >
             {/* <br /> */}
             {/* checkbox */}
-            <Box
-              gridColumnStart="1"
-              display="flex"
-              alignItems="center"
-              border="1px solid"
+            <Grid
+              gridColumn="1 / span 1"
+              placeContent="center"
+              border="1px transparent"
             >
-              {/* <Switch size="lg" /> */}
-              <input
+              <Switch
+                // colorScheme="green"
+                name="check"
+                size="lg"
+                isChecked={state}
+                onChange={(e) => handleCheckboxChange(id, e)}
+              />
+              {/* <input
                 type="checkbox"
                 checked={state}
                 onChange={(e) => handleCheckboxChange(id, e)}
-              />
-            </Box>
-            <Box
-              gridColumnStart="2"
-              // display="flex"
-              // alignItems="center"
-              border="1px solid"
+              /> */}
+            </Grid>
+            <Grid
+              gridColumn="2 / span 1"
+              alignItems="center"
+              border="1px transparent"
             >
-              <Text textAlign="left" paddingLeft={1} className="label">
+              <Text textAlign="left" paddingLeft={1}>
                 {title}
               </Text>
-            </Box>
-            <Box gridColumnStart="1" gridColumnEnd="3" border="1px solid">
-              <Text textAlign="left" className="lbl2">
-                {description}
-              </Text>
-            </Box>
+            </Grid>
+            <Grid
+              // alignContent="center"
+              gridColumn=" 1/ span 2"
+              border="1px transparent"
+            >
+              <Text textAlign="left">{description}</Text>
+            </Grid>
             {/*
              */}
-          </Box>
+          </Grid>
           {/* Label tarea */}
           {/* Label descripcion */}
           {/* <br /> */}
-        </Box>
-      </Box>
-      <div className={isVisible ? "content-hiden" : "content-show"}>
-        <form className="form" onSubmit={handleSubmit}>
-          <input
+        </Grid>
+      </Grid>
+      <Box mx={5} mt={5} display={!visibility ? "grid" : "none"}>
+        <form onSubmit={handleSubmit(handleSendSubmit)}>
+          {/* inputText */}
+          <FormControl id="title" isInvalid={errors.title}>
+            {/* <FormLabel>Renombrar tarea</FormLabel> */}
+            <Tooltip
+              label="Editar el titulo"
+              placement="top-start"
+              // isOpen
+              closeOnClick={false}
+            >
+              <span>
+                {/* Ícono de información que indica que hay un tooltip */}
+                <Input
+                  mb={errors.title ? "0px" : "5px"}
+                  placeholder="Editar tarea"
+                  {...register("title", {
+                    required: "Este campo es requerido",
+                    minLength: {
+                      value: 3,
+                      message: "La longitud minima es de 3 caracteres",
+                    },
+                  })}
+                />
+                <FormErrorMessage m={0}>
+                  {errors.title?.message}
+                </FormErrorMessage>
+              </span>
+            </Tooltip>
+          </FormControl>
+
+          <FormControl mt={2} id="description">
+            {/* <FormLabel>Renombrar tarea</FormLabel> */}
+            <Tooltip
+              label={
+                watch("description")
+                  ? "Editar la descripcion de esta tarea"
+                  : "Agregar una descripcion"
+              }
+              placement="top-start"
+              // isOpen
+              closeOnClick={false}
+            >
+              <span>
+                <Textarea
+                  size="s"
+                  rows={1} // Número de filas
+                  cols={30}
+                  placeholder="    Agregar una descripcion..."
+                  {...register("description")}
+                />
+              </span>
+            </Tooltip>
+          </FormControl>
+          {/* <input
             className="input"
             id="input-text"
             type="text"
@@ -128,28 +225,36 @@ export default function Task({ id, title, description, state }) {
             defaultValue={title}
             placeholder="Renombrar la tarea"
             onChange={handleOnChange}
-          />
-          <textarea
+          /> */}
+          {/* <textarea
             className="input"
             id="input-textarea"
             name="description"
             defaultValue={description}
             placeholder="Editar la descripcion"
             onChange={handleOnChange}
-          />
+          /> */}
           <br />
-          <button
-            className="button-cancel-edit"
+          <Button
+            variant="outline"
+            colorScheme="red"
             type="button"
+            mt={-2}
+            mr={1}
+            _hover={{
+              borderColor: "red.500",
+              color: "red.500",
+              // Cambia el color del borde al hacer hover
+            }}
             onClick={() => handleToggleVisibility()}
           >
             Cancelar
-          </button>
-          <button className="button-save-edit" type="submit">
+          </Button>
+          <Button mt={-2} colorScheme="teal" ml={1} type="submit">
             Guardar
-          </button>
+          </Button>
         </form>
-      </div>
-    </Box>
+      </Box>
+    </Grid>
   );
 }
